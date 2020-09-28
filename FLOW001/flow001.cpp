6662662f82b0d722db97d17eb34140bb414dad27
Adarsh Kumar <1805695@kiit.ac.in>
2020-09-28 12:35:28 +0530

    Add files via upload

---
+++ FLOW001/flow001.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt","r",stdin);
+	// freopen("output.txt","w",stdout);
+	// #endif
+
+	//Code starts here
+
+	int test_cases, A, B;
+	cin >> test_cases;
+
+	for(int i=0;i<test_cases;i++)
+	{
+		int sum=0;
+		cin >> A >> B;
+		sum = A+B;
+		cout << sum << endl;
+	}
+
+}
