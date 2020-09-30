b473615c9ec4afa6490f8a20a04cc925d18996d2
Adarsh Kumar <1805695@kiit.ac.in>
2020-09-30 11:14:42 +0530

    Add files via upload

---
+++ FLOW008/flow008.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt","r",stdin);
+	// freopen("output.txt","w",stdout);
+	// #endif
+
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		if (n<10)
+			cout << "Thanks for helping Chef!" <<endl;
+		else
+			cout << "-1" << endl;
+	}
+
+	return 0;
+}
