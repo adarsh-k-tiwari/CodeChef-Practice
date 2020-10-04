219c2d6583ab4da9031469cd698d43267bf3b780
Adarsh Kumar <1805695@kiit.ac.in>
2020-10-05 00:01:32 +0530

    Update diffsum.cpp

--- DIFFSUM/diffsum.cpp
+++ DIFFSUM/diffsum.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif;
+	
 	int a,b;
 	cin>> a>>b;
 	int sum=0,diff=0;
